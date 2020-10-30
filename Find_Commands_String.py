aa59c24cdefe6d36ec99991a002c5c77c622cba3
samuel <fsamuelsmartins@gmail.com>
2020-10-29 21:33:36 -0300

    separando o where e o sql inicial

--- Find_Commands_String.py
+++ Find_Commands_String.py
@@ -2,10 +2,38 @@ import re
 import numpy as np
 
 # querry = "select pessoa.nome,pessoa.idade from pessoa where pessa.idade = 20"
-querry = "select nome,idade,tipo_c from (select * from (select * from cliente join cartao on cartao.usuario = cliente.usuario),batata )"
+# querry = "select nome,idade,tipo_c from (select * from cliente join cartao on cartao.usuario = cliente.usuario),batata"
+querry = "SELECT LNAME FROM EMPLOYEE, WORKS_ON, PROJECT WHERE PNAME = ‘Aquarius’ OR project.PNUMBER = PNO AND ESSN = SSN AND BDATE > ‘1957-12-31’"
+querry = querry.lower()
+comandos = ["select ", " from ", " where ", " join ", " on ", "order by"]
+operadores_ilogicos = ["=", ">", "<", "<=", ">=", "<>"]
+operadores_logicos = [" and ", " or "," in ", " not in ", " like "]
 
-comandos = ["select", "from", "where", "join", "order by"]
-operadores = ["=", ">", "<", "<=", ">=", "<>", "And", "Or", "In", "Not In", "Like"]
+
+# PNAME = ‘Aquarius’ AND project.PNUMBER = PNO AND ESSN = SSN AND BDATE > ‘1957-12-31’
+
+def split_operators(operadores_ilogicos, operadores_logicos, query: str):
+    operadores_logicos_postions = []
+    for operador in operadores_logicos:
+        query = query.replace(operador, "!_!")
+        operadores_logicos_postions.extend([(m.start(), operador) for m in re.finditer(operador, querry)])
+
+    operadores_logicos_postions.sort(key=lambda tup: tup[0])
+    query = query.split("!_!")
+
+    conditions = []
+    for operador in operadores_ilogicos:
+        for i in query:
+            if operador in i:
+                infos = i.split(operador)
+                infos.append(operador)
+                conditions.append(tuple(infos))
+
+    print(operadores_logicos_postions)
+    print(conditions)
+
+    return operadores_logicos_postions,conditions
+
 
 select_postions = [m.start() for m in re.finditer("select", querry)]
 select_postions.reverse()
@@ -33,7 +61,7 @@ for i, postion in enumerate(select_postions):
 
     result["query"] = sub_querry
     for i, current_data in enumerate(sub_postions):
-        if i != len(sub_postions) -1:
+        if i != len(sub_postions) - 1:
             next_data = sub_postions[i + 1]
             result[current_data[0]] = sub_querry[current_data[1]: next_data[1]]
         else:
@@ -41,12 +69,15 @@ for i, postion in enumerate(select_postions):
 
     sub_querrys[key] = result
 
+# for key in sub_querrys:
+#     print(key)
+#     i = sub_querrys[key]
+#     for key2 in sub_querrys[key]:
+#         print(f"{key2}: {i[key2]}")
 
-for key in sub_querrys:
-    print(key)
-    i = sub_querrys[key]
-    for key2 in sub_querrys[key]:
-       print(f"{key2}: {i[key2]}")
+
+a = sub_querrys["!0!"][" where "].replace("where ", "")
+split_operators(operadores_ilogicos, operadores_logicos, a)
 
 # print(sub_querrys)
 # print(querry)
