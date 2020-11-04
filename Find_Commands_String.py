ad7e552380b4f29159a287f8e70014e844857ae3
samuel <fsamuelsmartins@gmail.com>
2020-11-03 21:06:44 -0300

    tudo quebrado

--- Find_Commands_String.py
+++ Find_Commands_String.py
@@ -3,10 +3,10 @@ import numpy as np
 
 # Setup --------------------------------------------------------
 
-query = "SELECT LNAME FROM EMPLOYEE, WORKS_ON, PROJECT WHERE project.PNAME = ‘Aquarius’ OR project.PNUMBER = PNO " \
-        "AND EMPLOYEE.ESSN = SSN AND WORKS_ON.BDATE > ‘1957-12-31’"
-# query = "select pessoa.nome,pessoa.idade from pessoa where pessa.idade = 20"
-# query = "select nome,idade,tipo_c from (select * from cliente join cartao on cartao.usuario = cliente.usuario),batata"
+# query = "SELECT LNAME FROM EMPLOYEE, WORKS_ON, PROJECT WHERE project.PNAME = Aquarius OR project.PNUMBER = PNO " \
+#         "AND EMPLOYEE.ESSN = SSN AND WORKS_ON.BDATE > 1957-12-31"
+query = "select pessoa.nome,pessoa.idade from pessoa where pessoa.sexo = m"
+# query = "select cliente.nome,cliente.idade,cartao.tipo_c from (select * from cliente join cartao on cartao.usuario = cliente.usuario),batata"
 
 query = query.lower()
 
@@ -18,12 +18,10 @@ op_logicos = [" and ", " or ", " in ", " not in ", " like "]
 # Metodos ------------------------------------------------------
 
 def split_operators(op_comp, op_logic, query: str):
-
     # Preenchendo vetor de operadores logicos (and, or, in, etc.) e os reorganizando
     op_logicos_positions = []
 
     for operador in op_logic:
-        # TODO / Os espaços foram removidos com o .strip() em operator
         op_logicos_positions.extend([(m.start(), operador.strip()) for m in re.finditer(operador, query)])
         query = query.replace(operador, "!_!")
 
@@ -50,9 +48,6 @@ def split_operators(op_comp, op_logic, query: str):
                 infos = i.split(operador)
                 infos.append(operador)
                 conditions.append(tuple(infos))
-
-    print(op_logicos_positions)
-    print(conditions)
 
     return op_logicos_positions, conditions
 
@@ -92,18 +87,17 @@ for i, position in enumerate(select_positions):
     for i, current_data in enumerate(sub_positions):
         if i != len(sub_positions) - 1:
             # TODO / Os espaços são removidos com o .strip() em current_data[0]. Ver com samuel o index_start abaixo.
-            #index_start = current_data[1] if sub_query[current_data[1]] != ' ' else (current_data[1] + 1)
+            # index_start = current_data[1] if sub_query[current_data[1]] != ' ' else (current_data[1] + 1)
             next_data = sub_positions[i + 1]
             result[current_data[0].strip()] = sub_query[current_data[1]: next_data[1]]
         else:
-            #index_start = current_data[1] if sub_query[current_data[1]] != ' ' else (current_data[1] + 1)
+            # index_start = current_data[1] if sub_query[current_data[1]] != ' ' else (current_data[1] + 1)
             result[current_data[0].strip()] = sub_query[current_data[1]:]
 
     # Dicionário de dicionários. Result é tanto a query toda quanto ela "dividida"
     sub_queries[key] = result
 
 print()
-
 # Printando cada select e subselect, junto com sua chave
 for key in sub_queries:
     print(key)
@@ -111,98 +105,75 @@ for key in sub_queries:
     for key2 in sub_queries[key]:
         print(f"{key2}: {i[key2]}")
 
-print()
-
 # Esqueci o que isso faz, perguntar ao Samuel. Mas nessa query não faz nada pq não tem where
 operators_dict = {}
 for key in sub_queries:
-    if " where " in sub_queries[key]:
-        a = sub_queries[key][" where "].replace("where ", "")
-        operators_dict[key] = split_operators(op_comparacao, op_logicos, a)
+    operators_dict[key] = {}
+    if "where" in sub_queries[key]:
+        a = sub_queries[key]["where"].replace("where ", "")
+        operators_dict[key]["where"] = split_operators(op_comparacao, op_logicos, a)
 
-print(operators_dict, end="\n\n")
+    if "on" in sub_queries[key]:
+        a = sub_queries[key]["on"].replace(" on ", "")
+        operators_dict[key]["on"] = split_operators(op_comparacao, op_logicos, a)
+
 print(sub_queries, end="\n\n")
+print(operators_dict, end="\n\n")
 
+relations = {}
 # Procurando todas as tabelas distintas e as colunas de cada tabela e as colocando no dicionário sub_queries
 for query in sub_queries:
-    sub_queries[query]["tabelas"] = ""
+    relations[query] = {}
+    relations[query]["tabelas"] = []
+    relations[query]["columns_info"] = {}
+    relations[query]["select_infos"] = {}
 
-    # Pegando tabelas a partir do from
+    if "select" in sub_queries[query]:
+        aux_array = sub_queries[query]["select"].replace("select ", '').replace(" ", '').split(",")
+
+        for tab in aux_array:
+            if tab != "*":
+                tabela_info, column = tab.split(".")
+                if tabela_info not in relations[query]["select_infos"]:
+                    relations[query]["select_infos"][tabela_info] = []
+                relations[query]["select_infos"][tabela_info].append(column)
+            else:
+                relations[query]["select_infos"]["all"] = [tab]
+
     if "from" in sub_queries[query]:
-        aux_array = sub_queries[query]["from"].split(' ')
-        aux_array = [i for i in aux_array if i]
+        aux_array = sub_queries[query]["from"].replace(" from ", '').replace(" ", '').split(",")
 
-        for tabela in aux_array[1:]:
-            if tabela not in sub_queries[query]["tabelas"]:
-                tabela.replace(',', '')
+        for tab in aux_array:
+            if tab not in relations[query]["tabelas"]:
+                relations[query]["tabelas"].append(tab)
 
-                if sub_queries[query]["tabelas"] != "":
-                    sub_queries[query]["tabelas"] += ","
-
-                sub_queries[query]["tabelas"] += tabela
-
-    # Pegando uma tabela a partir do join
     if "join" in sub_queries[query]:
-        aux_array = sub_queries[query]["join"].split(' ')
-        aux_array = [i for i in aux_array if i]
-        # TODO / Checar se tudo daqui pro próximo TODO está certo
-        tabela = aux_array[1]
+        aux_array = sub_queries[query]["join"].replace(" join ", '').replace(" ", '').split(",")
 
-        if tabela not in sub_queries[query]["tabelas"]:
-            tabela.replace(',', '')
+        for tab in aux_array:
+            if tab not in relations[query]["tabelas"]:
+                relations[query]["tabelas"].append(tab)
 
-            if sub_queries[query]["tabelas"] != "":
-                sub_queries[query]["tabelas"] += ","
+        a = operators_dict[query]["on"][1]
 
-            sub_queries[query]["tabelas"] += tabela
-        # TODO --------------------------------------------------
+        for tabela_info, value, _ in a:
+            tabela_info, column = tabela_info.split(".")
+            if tabela_info not in relations[query]["columns_info"]:
+                relations[query]["columns_info"][tabela_info] = []
 
-    # Pegando tabelas e colunas a partir do where
+            relations[query]["columns_info"][tabela_info].append(column)
+
+        # sub_queries[query]["on"].replace(" on ", '').replace(" ", '').split(",")
+
     if "where" in sub_queries[query]:
-        aux_array = sub_queries[query]["where"].split('.')
 
-        for i, element in enumerate(aux_array):
-            # Tabelas
-            if i != len(aux_array) - 1 and element.split(' ')[-1] not in sub_queries[query]["tabelas"]:
-                sub_queries[query]["tabelas"] += "," + element.split(' ')[-1]
+        a = operators_dict[query]["where"][1]
+        for tabela_info, value, _ in a:
+            tabela_info, column = tabela_info.split(".")
+            if tabela_info not in relations[query]["columns_info"]:
+                relations[query]["columns_info"][tabela_info] = []
 
-            # Colunas
-            if i != 0:
-                aux_tabela = aux_array[i-1].split(' ')[-1]
-                aux_coluna = element.split(' ')[0]
+            relations[query]["columns_info"][tabela_info].append(column)
 
-                # Se a tabela não existir como item do sub_queries, ela é adicionada
-                if ("t/" + aux_tabela) not in sub_queries[query]:
-                    sub_queries[query]["t/" + aux_tabela] = ""
-
-                # Adicionando a coluna da tabela associada no sub_queries
-                if aux_coluna not in sub_queries[query]["t/" + aux_tabela]:
-                    if sub_queries[query]["t/" + aux_tabela] != "":
-                        sub_queries[query]["t/" + aux_tabela] += ","
-
-                    sub_queries[query]["t/" + aux_tabela] += aux_coluna
-
-    # TODO select?
-
-print(sub_queries)
-
-# if "select" in sub_queries[query]:
-#     print(sub_queries[query]["select"])
-
-# --------------------------------------------------------------
-
-# print(sub_querrys)
-# print(querry)
-
-# for comando in comandos:
-#     print("----------------")
-#     print(comando)
-#     result = [m.start() for m in re.finditer(comando, sql)]
-#
-#     if len(result) > 0:
-#         positions.update({comando: result})
-#     print(result)
-
-# PNAME = ‘Aquarius’ AND project.PNUMBER = PNO AND ESSN = SSN AND BDATE > ‘1957-12-31’
-# query = " PNAME = ‘Aquarius’ AND project.PNUMBER = PNO !_! ESSN = SSN !_! BDATE > ‘1957-12-31’"
-# query = " PNAME = ‘Aquarius’ !_! project.PNUMBER = PNO !_! ESSN = SSN !_! BDATE > ‘1957-12-31’"
+print(relations)
+# Pegando tabelas e colunas a partir do where
