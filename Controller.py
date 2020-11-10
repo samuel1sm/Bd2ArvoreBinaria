a95fe5f82e24c9d4b9e536195eff2a924fb38d1d
samuel <fsamuelsmartins@gmail.com>
2020-11-09 21:09:02 -0300

    html criado e retorno preparado

--- Controller.py
+++ Controller.py
@@ -1,11 +1,17 @@
 from Find_Commands_String import verify_query
 from flask import Blueprint, jsonify, request
-
+import glob
+import os
+import random
 controller_api = Blueprint('controller_api', __name__)
 
 
-@controller_api.route('/verify_query', methods=['POST'])
-def get_verify_query():
-    query = request.get_json()["quary"]
+@controller_api.route('/post_key/<query>', methods=['POST'])
+def get_verify_query(query):
+    # query = request.get_json()["quary"]
     result = verify_query(query)
-    return jsonify(result), 200
+    print(f"{os.path.dirname(os.path.abspath(__file__))}/{glob.glob('*.png')[0]}")
+    result = random.sample('abcdefghijklmnopqrstuvxwyz', random.randint(5,20))
+    print(result)
+    return jsonify({"caminho" : f"{os.path.dirname(os.path.abspath(__file__))}/{glob.glob('*.png')[0]}", "execucao" : result}), 200
+    # return "200"
